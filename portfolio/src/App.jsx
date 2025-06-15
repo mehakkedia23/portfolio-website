639a280d84054a0701c569f2d72b3203ff2de390
mehakkedia23 <kediamehak756@gmail.com>
2025-06-15 14:31:14 +0530

    intial commit

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,24 @@
+import Hero from './components/Hero';
+import About from './components/About';
+import TechStack from './components/TechStack';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
+import BottomNavBar from './components/BottomNavBar';
+
+function App() {
+ 
+
+  return (
+    <>
+     <Hero/>
+     <About/>
+     <TechStack/>
+     <Projects/>
+     <Contact/>
+     <BottomNavBar/>
+
+    </>
+  )
+}
+
+export default App
---
+++ portfolio/src/components/Hero.jsx
@@ -0,0 +1,12 @@
+function Hero(){
+    return(
+        <div className="pt-32 pb-12 flex flex-col justify-center  items-center text-center bg-white">
+            <h4  className="italic text-black text-2xl ">Hello I'm</h4>
+            <h1 className="italic  text-6xl text-black font-bold mt-4  transition-colors duration-300 hover:text-purple-600">Mehak Kedia</h1>
+            <h3 className="italic text-2xl text-black  mt-2">Full Stack Developer | AI Enthusiast</h3>
+
+         </div>
+    )
+}
+
+export default Hero;
